51a285c5bbc86166ebb4a7e4fe534faae50b4136
Lehrfeld Artur <48105057+Tapselo@users.noreply.github.com>
2019-04-11 19:46:19 +0200

    improved operating order
    Validation made before splitting operations

--- SubnetCalc.py
+++ SubnetCalc.py
@@ -37,7 +37,6 @@ def splitting_the_octets(tab):
 def combining_into_octets(tab_list):
     tab=["","","",""]
     for j in range(0,8):
-        #tab[0].join(tab_list[j])
         tab[0] += str(tab_list[j])
     for j in range(8, 16):
         tab[1]+= str(tab_list[j])
@@ -71,7 +70,7 @@ def write_with_dots_to_file(file, tab):
 #------------------------------------------------Obsluga plikow
 file=open("wynik.txt",'w')
 #------------------------------------------------Organizacja danych
-print("Arguments: (IP,Netmask)")
+
 dane=['','']
 if(len(sys.argv)==1):
     hostname = socket.gethostname()
@@ -81,20 +80,20 @@ if(len(sys.argv)==1):
     mask=dane[1].split(".")
 else:
     dane=str(sys.argv[1])
-    dane= dane.split("/")
-    ip=dane[0].split(".")
-    mask=dane[1].split(".")
+    if "/" in dane:
+        dane=dane.split("/")
+        ip=dane[0].split(".")
+        mask=dane[1].split(".")
+    else:
+        print("Wrong ip/mask passed in arguments")
+        sys.exit(0)
+print("Arguments: (IP,Netmask)")
 print(dane)
 file.write("Data:")
 file.write("\nIP: "+dane[0])
-file.write("\nMask CIDR: "+dane[1])
-#------------------------------------------------Private/Public IP
-if(ip[0]=='10' or (ip[0]=='192'and ip[1]=='168') or (ip[0]=='172' and '16'<=ip[1]<='31') ):
-    print("\nIP is private")
-    file.write("\nIP is private")
-else:
-    print("\nIP is public")
-    file.write("\nIP is public")
+if(len(sys.argv)==2):
+    file.write("\nMask CIDR: "+dane[1])
+
 #------------------------------------------------WALIDACJA DANYCH
 i=0
 try:
@@ -102,12 +101,12 @@ try:
         ip[i] = int(x)
         i += 1
 except ValueError:
-    print("Niepoprawne dane")
+    print("Wrong arguments")
     sys.exit()
 
 if(len(sys.argv)!=1):
     if len(ip) != 4 or len(mask) != 1 :
-        print("IP musi posiadac 4 oktety, a maska tylko 1 lub 4!")
+        print("IP musi posiadac 4 oktety, a maska byc w formacie CIDR!")
         sys.exit()
     mask[0]=int(mask[0])
     for x in ip:
@@ -115,15 +114,23 @@ if(len(sys.argv)!=1):
             print("W IP podano liczbe ze zlego zakresu! (0-255)")
             sys.exit()
     if mask[0] < 0 or mask[0] > 24:
-        print("Maska powinna byc z zakresu: (1-24)")
+        print("Maska powinna byc z zakresu: (1-32)")
         sys.exit()
+#------------------------------------------------Private/Public IP
+if(ip[0]=='10' or (ip[0]=='192'and ip[1]=='168') or (ip[0]=='172' and '16'<=ip[1]<='31') ):
+    print("\nIP is private")
+    file.write("\nIP is private")
+else:
+    print("\nIP is public")
+    file.write("\nIP is public")
 
 #------------------------------------------------Zamiana na binarne
 ip=int_to_bin(ip)
 if(len(mask)==1):
     mask=cidr_to_netmask(mask[0])
     mask = mask.split(".")
-file.write("\n\nMask decimal: "+str(mask))
+file.write("\n\nMask decimal: ")
+write_with_dots_to_file(file,mask)
 i=0
 for x in mask:
     mask[i]=int(x)
@@ -134,7 +141,7 @@ print("\nMask binary:")
 print(mask)
 print("IP binary:")
 print(ip)
-file.write("\n\nMask binary: ")
+file.write("\nMask binary: ")
 write_with_dots_to_file(file,mask)
 file.write("\nIP binary: ")
 write_with_dots_to_file(file,ip)
@@ -156,7 +163,7 @@ file.write("\n\nNetwork binary: ")
 write_with_dots_to_file(file,network_octets)
 file.write("\nNetwork decimal: ")
 write_with_dots_to_file(file,network_decimal)
-#------------------------------------------------NetworkClass
+#------------------------------------------------NetworkClassaw
 klasa=""
 if(network[0]==0):
     klasa="A"
@@ -218,7 +225,7 @@ file.write("\n\nMax hosts in network: "+str(nrhosts))
 #------------------------------------------------Ping if host
 broadcast=combining_into_octets(broadcast)
 if(ip==network_octets or ip==broadcast):
-    print("IP is 'network' or 'broadcast'")
+    print("IP is a host")
 else:
     print("\nDo you want to ping this IP? y=yes, n=no")
     wybor=input()
